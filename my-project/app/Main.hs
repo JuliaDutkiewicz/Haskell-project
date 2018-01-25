4cd7ae9c7269b91b0d0a94461f65bce5f571c821
Julia Dutkiewicz <dutkiewicz.julia@gmail.com>
2018-01-25 20:00:30 +0100

    Julia's commit

--- my-project/app/Main.hs
+++ my-project/app/Main.hs
@@ -9,18 +9,31 @@ main :: IO ()
 main = do
   putStrLn "What's your name?"
   name <- getLine
-  print ("Hello " ++ name ++"!")
+  putStrLn ("Hello " ++ name ++"!")
+  putStrLn ("Here are some example commands:")
+  putStrLn ("--heapSort [1,345,2,325,24,4]--")
   print (heapSort [1,345,2,325,24,4])
+  putStrLn ("--isEmptyHH emptyHe--")
   print $ isEmptyHH emptyHe
+  putStrLn ("--isEmptyHH (makeHeapFromArray ([]::[Int]))--")
   print $ isEmptyHH (makeHeapFromArray ([]::[Int]))
+  putStrLn "--isEmptyHH (makeHeapFromArray [1,23,12,34])--"
   print $ isEmptyHH (makeHeapFromArray [1,23,12,34])
+  putStrLn "--topH (emptyHe) ::Maybe Int--"
   print $ (topH (emptyHe) ::Maybe Int)
+  putStrLn "--EmptyH :: Heap Char--"
   print (EmptyH :: Heap Char)
+  putStrLn "--topH $ makeHeapFromArray [1.3,2.4,1.2]--"
   print $ topH $ makeHeapFromArray [1.3,2.4,1.2]
+  putStrLn "--emptyQ :: Queue Char--"
   print (emptyQ :: Queue Char)
+  putStrLn "--(Queue [1,2,3]) :: Queue Integer--"
   print ((Queue [1,2,3]) :: Queue Integer)
+  putStrLn "--addQ 7 ((Queue [1,2]))--"
   print (addQ 7 ((Queue [1,2])))
+  putStrLn "--emptyDEQ :: Dequeue Int--"
   print (emptyDEQ :: Dequeue Int)
+  putStrLn "--(Queue [] :: Queue Int) == (Queue [] ::Queue Int)--"
   print ((Queue [] :: Queue Int) == (Queue [] ::Queue Int))
   print "Write your short list to sort it: "
   xs <- getLine
